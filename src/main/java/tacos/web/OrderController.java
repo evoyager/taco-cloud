4d7ffcddf4115cd9e3b61d7449b5e0b0a295fcb2
Ievgen Gusar <ievgen_gusar@epam.com>
2019-10-14 23:14:32 +0300

    Security added

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,9 @@
 package tacos.web;
 import javax.validation.Valid;
 
+import org.springframework.boot.actuate.trace.http.HttpTrace.Principal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import tacos.Order;
+import tacos.User;
 import tacos.data.OrderRepository;
+import tacos.data.UserRepository;
 
 @Controller
 @RequestMapping("/orders")
@@ -18,6 +23,7 @@ import tacos.data.OrderRepository;
 public class OrderController {
   
   private OrderRepository orderRepo;
+  private UserRepository userRepository;
 
   public OrderController(OrderRepository orderRepo) {
     this.orderRepo = orderRepo;
@@ -29,11 +35,13 @@ public class OrderController {
   }
 
   @PostMapping
-  public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+  public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus, 
+		  @AuthenticationPrincipal User user) {
     if (errors.hasErrors()) {
       return "orderForm";
     }
     
+    order.setUser(user);
     orderRepo.save(order);
     sessionStatus.setComplete();
     
